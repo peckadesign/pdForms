06c63fad95277cf3b1aa6445887f150da52b9a74
Lukáš Křehula <lukas.krehula@peckadesign.cz>
2021-03-24 11:40:33 +0100

    Povýšení verze pdForms na v3.4.2

--- src/assets/pdForms.js
+++ src/assets/pdForms.js
@@ -1,7 +1,7 @@
 /**
  * @name pdForms
  * @author Radek Šerý <radek.sery@peckadesign.cz>
- * @version 3.3.0
+ * @version 3.4.2
  *
  * Features:
  * - live validation
@@ -45,7 +45,7 @@
 
 	var pdForms = window.pdForms || {};
 
-	pdForms.version = '3.3.0';
+	pdForms.version = '3.4.2';
 
 
 	/**
