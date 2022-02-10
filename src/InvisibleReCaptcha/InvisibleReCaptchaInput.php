0c3971f79fe0a5807e80afbca5a6864118d89d4a
honzaJK <jan.klima@peckadesign.cz>
2022-02-10 14:10:44 +0100

    Přidání nonce attributu pro scripty do recaptcha inputu
    - uprava return typu phpstan 8

--- src/InvisibleReCaptcha/InvisibleReCaptchaInput.php
+++ src/InvisibleReCaptcha/InvisibleReCaptchaInput.php
@@ -28,9 +28,6 @@ class InvisibleReCaptchaInput extends \Contributte\ReCaptcha\Forms\ReCaptchaFiel
 	}
 
 
-	/**
-	 * @return \Nette\Utils\Html<\Nette\Utils\Html|string>
-	 */
 	public function getControl(): \Nette\Utils\Html
 	{
 		$formHtmlId = \sprintf(\Nette\Forms\Controls\BaseControl::$idMask, $this->form->lookupPath()); //pozor: musi se volat az zde, protoze teprve az ted je formular pripojen do nejake komponenty a muzeme volat lookupPath().
@@ -58,9 +55,6 @@ class InvisibleReCaptchaInput extends \Contributte\ReCaptcha\Forms\ReCaptchaFiel
 	}
 
 
-	/**
-	 * @return \Nette\Utils\Html<\Nette\Utils\Html|string>
-	 */
 	private function getReCaptchaScript(): \Nette\Utils\Html
 	{
 		$script = \Nette\Utils\Html::el('script', [
@@ -72,9 +66,6 @@ class InvisibleReCaptchaInput extends \Contributte\ReCaptcha\Forms\ReCaptchaFiel
 	}
 
 
-	/**
-	 * @return \Nette\Utils\Html<\Nette\Utils\Html|string>
-	 */
 	private function getReCaptchaInitScript(string $formHtmlId): \Nette\Utils\Html
 	{
 		$script = \Nette\Utils\Html::el('script')->setHtml('if (typeof pdForms !== "undefined" && pdForms.recaptcha) { pdForms.recaptcha.initForm("' . $formHtmlId . '"); }');
@@ -83,9 +74,6 @@ class InvisibleReCaptchaInput extends \Contributte\ReCaptcha\Forms\ReCaptchaFiel
 	}
 
 
-	/**
-	 * @return \Nette\Utils\Html<\Nette\Utils\Html|string>
-	 */
 	private function getReCaptchaDiv(): \Nette\Utils\Html
 	{
 		$div = \Nette\Utils\Html::el('div');
