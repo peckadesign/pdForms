bed4d4cf863717e0ce9979a98eeb9b730f9ad58d
Jakub-Fajkus <jakub.fajkus@gmail.com>
2022-03-29 09:47:10 +0200

    Bump js version to 3.5.0

--- src/assets/pdForms.js
+++ src/assets/pdForms.js
@@ -1,7 +1,7 @@
 /**
  * @name pdForms
  * @author Radek Šerý <radek.sery@peckadesign.cz>
- * @version 3.4.4
+ * @version 3.5.0
  *
  * Features:
  * - live validation
@@ -45,7 +45,7 @@
 
 	var pdForms = window.pdForms || {};
 
-	pdForms.version = '3.4.4';
+	pdForms.version = '3.5.0';
 
 
 	/**
