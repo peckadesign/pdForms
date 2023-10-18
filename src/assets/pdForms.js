64e1131d9ece3796fcc2b0551bc7fda02e6cafca
Radek Šerý <radek.sery@peckadesign.cz>
2023-10-18 10:10:41 +0200

    Bump verze dle backend části

--- src/assets/pdForms.js
+++ src/assets/pdForms.js
@@ -1,7 +1,7 @@
 /**
  * @name pdForms
  * @author Radek Šerý <radek.sery@peckadesign.cz>
- * @version 3.6.2
+ * @version 4.0.1
  *
  * Features:
  * - live validation
@@ -45,7 +45,7 @@
 
 	var pdForms = window.pdForms || {};
 
-	pdForms.version = '3.6.2';
+	pdForms.version = '4.0.1';
 
 
 	/**
