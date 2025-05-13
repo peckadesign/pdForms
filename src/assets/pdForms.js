924e0511d7f36d158284a16a5cfe877a80b42240
Radek Šerý <radek.sery@peckadesign.cz>
2025-05-13 09:08:35 +0200

    Release 4.2.1

--- src/assets/pdForms.js
+++ src/assets/pdForms.js
@@ -1,7 +1,7 @@
 /**
  * @name pdForms
  * @author Radek Šerý <radek.sery@peckadesign.cz>
- * @version 4.2.0
+ * @version 4.2.1
  *
  * Features:
  * - live validation
@@ -45,7 +45,7 @@
 
 	var pdForms = window.pdForms || {};
 
-	pdForms.version = '4.2.0';
+	pdForms.version = '4.2.1';
 
 
 	/**
